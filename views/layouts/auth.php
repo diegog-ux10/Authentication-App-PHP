d3ffc5c9155bfe476aac8a27a3a21c9ecafea04f
diegogranados143@gmail.com <diego@codeando.edu.pe>
2023-09-13 12:51:41 -0400

    wip

--- views/layouts/auth.php
+++ views/layouts/auth.php
@@ -1,13 +1,26 @@
+<?php use core\Application; ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@400;600&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <link rel="stylesheet" href="../../assets/base.css">
 </head>
 <body>
+  <div class="main-container_auth">
+    <?php if(Application::$app->session->getFlash("success")): ?>
+    <div class="aler alert-sucess">
+      <?php echo Application::$app->session->getFlash("success") ?>
+    </div>
+    <?php endif ?>
     {{content}}
+  </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
 </body>
 </html>
